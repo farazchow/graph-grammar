7748151058095648d24eb79ad61e7deb1db96df3
Faraz Chowdhury <farazcho@mit.edu>
2024-04-24 17:03:49 -0400

    fixed test case for String parser

--- src/tests/unit/createRule.test.ts
+++ src/tests/unit/createRule.test.ts
@@ -17,10 +17,10 @@ describe("test createRuleFromString", () => {
     const d_1 = new GraphNode("d");
 
     const adjacencyMatrix = new Map();
-    adjacencyMatrix.set(b_1, new Set([d_1, c_1]));
-    adjacencyMatrix.set(b_2, new Set([c_1]));
-    adjacencyMatrix.set(c_1, new Set([b_2, b_1]));
-    adjacencyMatrix.set(d_1, new Set([b_1]));
+    adjacencyMatrix.set(b_1, [d_1, c_1]);
+    adjacencyMatrix.set(b_2, [c_1]);
+    adjacencyMatrix.set(c_1, [b_2, b_1]);
+    adjacencyMatrix.set(d_1, [b_1]);
     const graph = new Graph(adjacencyMatrix);
     expect(graph.isEqual(rule.right)).toBe(true);
   });
